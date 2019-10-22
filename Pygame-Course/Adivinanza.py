bc42d36d794608aa94a722ae326abd1ef4ecfe09
IsmaBH <deigo-b@hotmail.com>
2019-10-22 00:35:16 -0500

    Primer juego Juego de adivinanza de un numero random dado por la maquina

---
+++ Pygame-Course/Adivinanza.py
@@ -0,0 +1,27 @@
+#Juego de adivinar un numero
+import random
+#Variables
+intentos = 0;
+#Saludo personalizado
+print("Hola, ¿como te llamas?")
+nombre = input()
+print("Muy bien, "+nombre+", he pensado un numero entre 1 y 100, A ver si lo adivinas")
+#Generamos un numero aleatorio
+numero = random.randint(1,100)
+#Inicio del bucle
+while intentos < 10:
+	numero_adivinar = ''
+	numero_adivinar = input()
+	numero_adivinar = int(numero_adivinar)
+
+	if numero_adivinar < numero:
+		print("El numero que he pensado es MAYOR")
+	if numero_adivinar > numero:
+		print("El numero que he pensado es MENOR")
+	if numero_adivinar == numero:
+		print("Bien hecho, haz adivinado el numero que pense!!")
+		print("Haz acertado en "+str(intentos)+" intentos")
+		break;
+	intentos = intentos + 1
+if intentos >= 10:
+	print("Lo siento no haz acertado, el numero que pense era "+str(numero))
