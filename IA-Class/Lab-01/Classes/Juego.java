7ac18353217f90718b17381d26c7d26087d43636
IsmaBH <deigo-b@hotmail.com>
2021-04-04 20:27:56 -0500

    Implementacion base del juego de laberinto

--- IA-Class/Lab-01/Classes/Juego.java
+++ IA-Class/Lab-01/Classes/Juego.java
@@ -3,6 +3,51 @@
 ** Clases Laberinto y Personaje
 */
 
-public class Juego{
-	//Pass
+import java.awt.Graphics;
+import java.awt.event.KeyListener;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Juego extends JPanel{
+	//Atributos de la clase
+	Laberinto laberinto = new Laberinto();
+	Personaje personaje = new Personaje();
+	//Constructor de la clase
+	public Juego(){
+		addKeyListener(new KeyListener(){
+			@Override
+			public void keyTyped(KeyEvent e){}
+			@Override
+			public void keyPressed(KeyEvent e){
+				personaje.teclaPresionada(e);
+			}
+			@Override
+			public void keyReleased(KeyEvent e){}
+		});
+		setFocusable(true);
+	}
+	//Metodos de la clase
+	@Override
+	public void paint(Graphics grafico){
+		//Empieza el metodo que nos permitira dibujar el laberinto
+		laberinto.paint(grafico);
+		personaje.paint(grafico);
+	}
+	public static void main(String[] args){
+		JFrame miventana = new JFrame("Mi laberinto");
+		Juego game = new Juego();
+		miventana.add(game);
+		miventana.setSize(920,540);
+		miventana.setLocation(300,200);
+		miventana.setVisible(true);
+		miventana.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		while(true){
+			try{
+				Thread.sleep(10);
+			}catch(InterruptedException ex){
+				System.out.println("Ocurrio un error");
+			}
+			game.repaint();
+		}
+	}
 }
--- IA-Class/Lab-01/Classes/Personaje.java
+++ IA-Class/Lab-01/Classes/Personaje.java
@@ -3,6 +3,50 @@
 ** con sus caracteristicas
 */
 
+import java.awt.Graphics;
+import java.awt.event.KeyEvent;
+
 public class Personaje{
-	//Pass
+	//Atributos de la clase
+	Laberinto lab = new Laberinto();
+	private int x = 40;
+	private int y = 40;
+	private final int ancho = 40;
+	private final int alto = 40;
+	private final int movimiento = 40;
+	//Metodos de la clase
+	@Override
+	public void paint(Graphics grafico){
+		grafico.setColor(Color.red);
+		grafico.fillOval(x,y,ancho,alto);
+		grafico.SetColor(Color.black);
+		grafico.drawOval(x,y,ancho,alto);
+	}
+	public void teclaPresionada(KeyEvent evento){
+		int laberinto[][] = lab.obtieneLaberinto();
+		//Tecla izquierda
+		if(evento.getKeyCode() == 37){
+			if(laberinto[y/40][(x/40)-1] != 1){
+				x = x - movimento;
+			}
+		}
+		//Tecla derecha
+		if(evento.getKeyCode() == 39){
+			if(laberinto[y/40][(x/40)+1] != 1){
+				x = x + movimento;
+			}
+		}
+		//Tecla abajo
+		if(evento.getKeyCode() == 40){
+			if(laberinto[(y/40)+1][x/40] != 1){
+				y = y + movimento;
+			}
+		}
+		//Tecla arriba
+		if(evento.getKeyCode() == 38){
+			if(laberinto[(y/40)-1][x/40] != 1){
+				y = y - movimiento;
+			}
+		}
+	}
 }
