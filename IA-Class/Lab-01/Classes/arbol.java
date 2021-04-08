37f36c376c76c5cdb55d5074438f3d2a314f233c
IsmaBH <deigo-b@hotmail.com>
2021-04-08 17:56:43 -0500

    Reorganizacion de los ficheros de las estructuras

--- IA-Class/Lab-01/Classes/arbol.java
+++ IA-Class/Lab-01/Classes/arbol.java
@@ -1,34 +1,86 @@
 //Clase para implementar el arbol
-class arbol{
+import java.util.Vector;
+
+public class arbol{
 	Nodo raiz;
-	public Nodo insertarRaiz(String dato){
-		raiz = new Nodo(dato);
+	public Nodo insertarRaiz(String dato,int x,int y){
+		raiz = new Nodo(dato,x,y);
 		return raiz;
 	}
-	public void verHijosRecursivo(Nodo nodo){
+	public void verHijos(Nodo nodo){
 		//Se inicia con los padres
 		for(int i = 0;i<nodo.nohijos;i++){
 			nodo.hijos[i].verNodo();
 			//Se hace lo mismo con los hijos
-			verHijosRecursivo(nodo.hijos[i]);
+			verHijos(nodo.hijos[i]);
 		}
 	}
-	public void InsertarRecursivo(Nodo nodo,String dato,String padre){
-		Nodo nuevo = new Nodo(dato);
+	public void Insertar(Nodo nodo,String dato,int x,int y,String padre){
+		Nodo nuevo = new Nodo(dato,x,y);
 		//Si el padre es la raiz
-		if(nodo.getDato().equals(padre)){
+		if(nodo.getId().equals(padre)){
 			nodo.aumentarHijos(nuevo);
 		}else{
 			//Si el padre es uno de los hijos
 			for(int i = 0;i<nodo.nohijos;i++){
-				if(nodo.hijos[i].getDato().equals(padre)){
+				if(nodo.hijos[i].getId().equals(padre)){
 					//Se coloca el hijo en el nodo
 					nodo.hijos[i].aumentarHijos(nuevo);
 				}else{
 					//Busca el padre en los hijos del nodo
-					InsertarRecursivo(nodo.hijos[i],dato,padre);
+					Insertar(nodo.hijos[i],dato,x,y,padre);
 				}
 			}
 		}
+	}
+}
+
+class Nodo{
+	String id;
+	int coordX = 0;
+	int coordY = 0;
+	int nohijos;
+	Nodo hijos[];
+	Nodo hijosT[];
+
+	public Nodo(String id,int x,int y){
+		this.id = id;
+		this.coordX = x;
+		this.coordY = y;
+		this.nohijos = 0;
+	}
+	public void copiarHijos(){
+		//Aumenta en 1 los hijos con un arreglo temporal
+		this.hijosT = new Nodo[nohijos+1];
+		for(int i = 0;i<this.nohijos;i++){
+			hijosT[i] = hijos[i];
+		}
+	}
+	public void aumentarHijos(Nodo nodo){
+		copiarHijos();
+		this.hijosT[this.nohijos] = nodo;
+		this.hijos = hijosT;
+		this.nohijos++;
+	}
+	public String getId(){
+		return this.id;
+	}
+	public void setId(String dato){
+		this.id = dato;
+	}
+	public int getX(){
+		return this.coordX;
+	}
+	public void setX(int dato){
+		this.coordX = dato;
+	}
+	public int getY(){
+		return this.coordY;
+	}
+	public void setY(int dato){
+		this.coordY = dato;
+	}
+	public void verNodo(){
+		System.out.println("{"+id+"}");
 	}
 }
--- IA-Class/Lab-01/Classes/listaDoble.java
+++ IA-Class/Lab-01/Classes/listaDoble.java
@@ -50,3 +50,50 @@ public class listaDoble{
         System.out.println();
     }
 }
+class Node {
+    private int coorX;
+    private int coorY;
+    private Node nextElement;
+    private Node previousElement;
+ 
+    public Node(int x,int y) {
+        this.coorX = x;
+        this.coorY = y;
+    }
+ 
+    public Integer getValueX() {
+        return coorX;
+    }
+
+    public Integer getValueY() {
+        return coorY;
+    }
+
+    public void setValueX(Integer value) {
+        this.coorX = value;
+    }
+
+    public void setValueY(Integer value) {
+        this.coorY = value;
+    }
+ 
+    public Node getNextElement() {
+        return nextElement;
+    }
+ 
+    public void setNextElement(Node nextElement) {
+        this.nextElement = nextElement;
+    }
+ 
+    public Node getPreviousElement() {
+        return previousElement;
+    }
+ 
+    public void setPreviousElement(Node previousElement) {
+        this.previousElement = previousElement;
+    }
+    @Override
+    public String toString() {
+        return "Node [values= " +coorX+","+coorY+"]";
+    }
+}
